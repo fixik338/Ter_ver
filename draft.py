dfda26c3a07951bc8507633e8788085109ff76e7
fixik338 <0firter0@gmail.com>
2020-02-29 17:22:46 +0400

    123

--- draft.py
+++ draft.py
@@ -2,18 +2,14 @@ import numpy as np
 from math import *
 
 while True:
-    kel = int(input('количество элементов = '))
-    kn = int(input('типов (не больше ' + str(kel) + ') = '))
-    n = int(input('берут с ящика (не больше ' + str(kel) + ') = '))
+    kn = int(input("Кол - во типов"))
     k = np.zeros(kn)
     for i in range(kn):
-        if np.sum(k) < kel:
-            k[i] = int(input(' k[' + str(i) + '] = '))
-        else:
-            break
+            k[i+1] = int(input(' k[' + str(i) + '] = '))
+    kel = np.sum(k)
+    n = int(input('берут с ящика (не больше ' + str(kel) + ') = '))
     nFind = int(input('Сколько элементов (не больше ' + str(n) + ') = '))
     kFind = int(input('Какой тип (не больше ' + str(kn) + ') = '))
     C = (factorial(k[kFind]) / (factorial(nFind) * factorial(k[kFind] - nFind)))
     Cob = factorial(kel) / (factorial(n) * factorial(kel - n))
-    P = factorial(kel)
     print(C,'\n', Cob,'\n', C/Cob)
