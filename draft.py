f2192c3245df0708fd2b89cd452a05e4c7c55981
fixik338 <0firter0@gmail.com>
2020-02-29 17:57:26 +0400

    Исправлено условие нулевой вероятности

--- draft.py
+++ draft.py
@@ -5,14 +5,14 @@ while True:
     kn = int(input("Кол - во типов = "))
     k = np.zeros(kn)
     for i in range(kn):
-            k[i] = int(input(' k[' + str(i) + '] = '))
+        k[i] = int(input(' k[' + str(i) + '] = '))
     kel = int(np.sum(k))
     n = int(input('берут с ящика (не больше ' + str(kel) + ') = '))
     nFind = int(input('Сколько элементов (не больше ' + str(n) + ') = '))
     kFind = int(input('Какой тип (не больше ' + str(kn) + ') = '))
-    if nFind <= kFind:
+    if nFind <= k[kFind]:
         C = factorial(k[kFind]) / (factorial(nFind) * factorial(k[kFind] - nFind))
         Cob = factorial(kel) / (factorial(n) * factorial(kel - n))
-        print(C,'\n', Cob,'\n', C/Cob)
+        print(C, '\n', Cob, '\n', C / Cob)
     else:
         print(0)
