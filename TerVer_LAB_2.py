c8aa038efe6e1644ad55b1adabbace0ac36150b6
fixik338 <0firter0@gmail.com>
2020-03-03 14:57:10 +0400

    Добавлена задача со стрелками

--- TerVer_LAB_2.py
+++ TerVer_LAB_2.py
@@ -1,2 +1,67 @@
 import numpy as np
 from math import *
+def I_Shooter(p,q):
+    s = int(input("Номер стрелка = "))
+    q[s] = p[s]
+    c = 1
+    for i in range(1,6):
+        c *= q[i]
+    return c
+def OnlyTwoShooters(p, q):
+
+
+def OnlyOneShooter(p, q):
+    c = []
+    qs = q
+    for i in range(1,6):
+        qs[i] = p[i]
+        c.append(qs[1]*qs[2]*qs[3]*qs[4]*qs[5])
+        qs[i] = q[i]
+    return sum(c)
+
+
+def Noless4Shooters(p, q):
+
+
+
+def AtLeast1Shooter(p, q):
+    return sum(p)
+
+
+def terver(t='0'):
+    if t == '1':
+        q = []
+        p = []
+        for i in range(1, 7):
+            q.append(float(input('q[' + str(i) + '] = ')))
+            p.append(1 - q[i])
+        P = p[1]*(p[2]+(p[4]*p[5]*p[6]))*p[3]
+        return P
+    if t == '2':
+        p = []
+        q = []
+        for i in range(1, 6):
+            p.append(float(input('p[' + str(i) + '] = ')))
+            q.append(1-p[i])
+        while True:
+            print("Выберите пункт:\n "
+                  "a - только i-го стрелка\n "
+                  "b - только одного стрелка\n "
+                  "c - только двух стрелков\n "
+                  "d - не менее четырех стрелков\n "
+                  "e - хотя бы одного стрелка.")
+            g = input()
+            if g == 'a':
+                print(I_Shooter(p, q))
+            elif g == 'b':
+                print(OnlyOneShooter(p, q))
+            elif g == 'c':
+                print(OnlyTwoShooters(p, q))
+            elif g == 'd':
+                print(Noless4Shooters(p, q))
+            elif g == 'e':
+                print(AtLeast1Shooter(p, q))
+"------------------------------------------"
+while True:
+   g = input()
+   print(terver(g))
